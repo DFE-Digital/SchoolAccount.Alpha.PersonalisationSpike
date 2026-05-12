da95dc6cbee5c65a693ab987588cc59d2ae4596a
Mark Harrop <mark.harrop@education.gov.uk>
2026-05-12 10:43:02 +0100

    split up classes

--- SchoolAccount.Alpha/Services/GovUkSearchService.cs
+++ SchoolAccount.Alpha/Services/GovUkSearchService.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.WebUtilities;
-using SchoolAccount.Alpha.Converters;
+using SchoolAccount.Alpha.Services.Models;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace SchoolAccount.Alpha.Services
 {
@@ -31,46 +30,27 @@ namespace SchoolAccount.Alpha.Services
 
         private async Task<GovUkSearchResults?> DeserializeResponse(HttpResponseMessage response)
         {
-            return await response.Content.ReadFromJsonAsync<GovUkSearchResults>(GetJsonOptions());
-        }
-
-
-        private JsonSerializerOptions GetJsonOptions()
-        {
-            var options = new JsonSerializerOptions
+            
+            var results = await response.Content.ReadFromJsonAsync<GovUkSearchResults>();
+            
+            // Post-process to convert taxon IDs to names
+            if (results?.Results != null)
             {
-                PropertyNameCaseInsensitive = true
-            };
-            options.Converters.Add(new TaxonListConverter(taxonService));
-            return options;
+                foreach (var item in results.Results)
+                {
+                    item.Taxons = item.Taxons
+                        .Select(taxonService.GetTaxonName)
+                        .Where(name => !string.IsNullOrEmpty(name))
+                        .ToList();
+                        
+                    item.PartOfTaxonomyTree = item.PartOfTaxonomyTree
+                        .Select(taxonService.GetTaxonName)
+                        .Where(name => !string.IsNullOrEmpty(name))
+                        .ToList();
+                }
+            }
+            
+            return results;
         }
     }
-
-    public class GovUkSearchResults
-    {
-        public int Total { get; set; }
-        public int Start { get; set; }
-        public List<GovUkSearchResultItem> Results { get; set; } = new ();
-    }
-
-    public class GovUkSearchResultItem
-    {
-        [JsonPropertyName("title")]
-        public string Title { get; set; } = string.Empty;
-
-        [JsonPropertyName("description")]
-        public string Description { get; set; } = string.Empty;
-        [JsonPropertyName("format")]
-        public string Format { get; set; } = string.Empty;
-        [JsonPropertyName("public_updated_at")]
-        public string UpdatedAt { get; set; } = string.Empty;
-        [JsonPropertyName("link")]
-        public string Link { get; set; } = string.Empty;
-        [JsonPropertyName("taxons")]
-        public List<string> Taxons { get; set; } = new();
-        [JsonPropertyName("part_of_taxonomy_tree")]
-        public List<string> PartOfTaxonomyTree { get; set; } = new();
-
-    }
-
 }
---
+++ SchoolAccount.Alpha/Services/Models/GovUkSearchResultItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SchoolAccount.Alpha.Services.Models;
+
+public class GovUkSearchResultItem
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; } = string.Empty;
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; } = string.Empty;
+    [JsonPropertyName("format")]
+    public string Format { get; set; } = string.Empty;
+    [JsonPropertyName("public_updated_at")]
+    public string UpdatedAt { get; set; } = string.Empty;
+    [JsonPropertyName("link")]
+    public string Link { get; set; } = string.Empty;
+    [JsonPropertyName("taxons")]
+    public List<string> Taxons { get; set; } = new();
+    [JsonPropertyName("part_of_taxonomy_tree")]
+    public List<string> PartOfTaxonomyTree { get; set; } = new();
+
+}
